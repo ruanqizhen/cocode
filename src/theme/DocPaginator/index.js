5221973b159aa242e0548e8c00d1165dd313ab91
ruanqizhen <ruanqizhen@gmail.com>
2026-05-24 20:26:03 -0400

    Update index.js

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
 import Giscus from '@giscus/react';
-import {useColorMode} from '@docusaurus/theme-common';
+import { useColorMode } from '@docusaurus/theme-common';
 
 export default function DocPaginatorWrapper(props) {
-  const {colorMode} = useColorMode();
+	const { colorMode } = useColorMode();
 
-  return (
-    <>
-      <DocPaginator {...props} />
-	  <br />
-	  <Giscus
 		repo='ruanqizhen/cocode'
-		repoId='R_kgDOKhrU6g'
-		category='Announcements'
-		categoryId='DIC_kwDOKhrU6s4CaOFt'
-		mapping='pathname'
-		strict='1'
-		reactionsEnabled='1'
-		emitMetadata='1'
-		inputPosition='top'
-		theme={colorMode}
-		lang='zh-CN'
-        loading="lazy"
-	  />
-    </>
-  );
+	return (
+		<>
+			<DocPaginator {...props} />
+			<br />
+			<Giscus
+				repo='ruanqizhen/cocode'
+				repoId='R_kgDOSm0Rhg'
+				category='Announcements'
+				categoryId='DIC_kwDOSm0Rhs4C9xJS'
+				mapping='pathname'
+				strict='1'
+				reactionsEnabled='1'
+				emitMetadata='1'
+				inputPosition='top'
+				theme={colorMode}
+				lang='zh-CN'
+				loading="lazy"
+			/>
+		</>
+	);
 }
